a113b03c87f166a60b0b6318b26a451fd8c40ff9
Michael Kruse <llvm-project@meinersbur.de>
2025-01-09 19:42:03 +0100

    Post-merge fixes

--- flang-rt/unittests/Runtime/CUDA/AllocatorCUF.cpp
+++ flang-rt/unittests/Runtime/CUDA/AllocatorCUF.cpp
@@ -12,10 +12,9 @@
 #include "flang-rt/CUDA/allocator.h"
 #include "flang-rt/allocator-registry.h"
 #include "flang-rt/descriptor.h"
+#include "flang-rt/memory.h"
 #include "flang/Runtime/CUDA/descriptor.h"
 #include "flang/Runtime/allocatable.h"
-#include "flang/Runtime/descriptor.h"
-#include "flang/Runtime/memory.h"
 #include "flang/Support/Fortran.h"
 
 using namespace Fortran::runtime;
